434aacced1e53d049faee1d54169e96fd9c3c486
TimurMingaziev <tminraz@gmail.com>
2016-06-06 15:28:28 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,18 @@ public class Main {
         ArrayList<Student> list = new ArrayList();
 
         while (true) {
-            Thread.sleep(100);
-        //    System.out.println(list.size());
+       //   Thread.sleep(1);
+            System.out.println(list.size());
 
 
 
-            if (i % 1000 == 0) {
-                if(i%1000000==0)
+            if (i % 10000 == 0) {
+                if(i%5000000==0)
                      list.clear();
                 else {
                     int endsize = list.size() * 90 / 100;
-                    for (int y = 0; y < endsize; y++)
-                        list.remove(list.size() - 1);
+//                    for (int y = 0; y < endsize; y++)
+//                        list.remove(list.size() - 1);
                 }
 
             } else {
