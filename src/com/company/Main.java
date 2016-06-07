30ab306914f3aaab9a9b1a283260025aa2c2ae54
TimurMingaziev <tminraz@gmail.com>
2016-06-07 13:36:43 +0300

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,41 +3,64 @@ package com.company;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Timer;
+import java.util.function.DoubleToIntFunction;
 
 
 public class Main {
     private static final Logger LOG = LoggerFactory.getLogger("logfile");
 
     public static void main(String[] args) throws InterruptedException {
-        int i = 0;
-        Timer time = new Timer();
+        long i = 0;
+        long maxIt = 0;
+        long midle=0;
+        long estimatedTime=0;
 
-        LOG.info("looool");
-        ArrayList<Student> list = new ArrayList();
+
+        ArrayList<Integer> list = new ArrayList();
+        ArrayList<Boolean> listBool = new ArrayList();
+        ArrayList<BigInteger> listBig = new ArrayList();
+        ArrayList<String> listStr = new ArrayList();
+        ArrayList<Student> listStud = new ArrayList();
+
 
         while (true) {
-          Thread.sleep(1000);
+          Thread.sleep(100);
+            long startTime = System.nanoTime();
             System.out.println(list.size());
 
-
-
-            if (i % 10000 == 0) {
-                if(i%5000000==0)
+            if (i % 1000 == 0) {
+                if(i%100000==0)
                      list.clear();
                 else {
                     int endsize = list.size() * 90 / 100;
-//                    for (int y = 0; y < endsize; y++)
-//                        list.remove(list.size() - 1);
+                    for (int y = 0; y < endsize; y++)
+                        list.remove(list.size() - 1);
                 }
 
             } else {
 
-                list.add(new Student(i, Math.random()+"fffffffffffff",Math.random()));
+                list.add(new Integer((int)Math.random()*1000));
+                listBool.add(new Boolean(true));
+                listBig.add(new BigInteger("weffffffffffffffsdfsf" + Math.random()));
+                listStr.add(new String("lol" + Math.random()));
+                listStud.add(new Student("name" + Math.random(),"life" + Math.random()));
 
             }
             i++;
+            estimatedTime = System.nanoTime() - startTime;
+            midle += estimatedTime;
+            LOG.info("Time of  " + i +" iteration = " +estimatedTime);
+
+            if(maxIt<estimatedTime)
+            {
+                maxIt=estimatedTime;
+                LOG.info("MAAAAAAAAAAAAAAAAAAAAX  " + i +" iteration = " +maxIt);
+            }
+
+            LOG.info("Midle  on " + i +" iteration = " + midle/i);
+
         }
 
 
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -10,15 +10,15 @@ import java.util.Date;
  * Created by Home on 05.06.2016.
  */
 public class Student extends ArrayList {
-    int name=0;
+
     String fio ="";
-    double life=0;
+    String life= "";
 
     public Student(){
         System.out.println("student created");
     }
-    public Student( int name, String fio, double life){
-        this.name = name;
+    public Student( String fio, String life){
+
         this.fio = fio;
         this.life = life;
 
